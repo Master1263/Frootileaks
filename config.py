737bf6bafea1ec904c31c8d6cd62190192e30379
Master1263 <master1262yt@gmail.com>
2024-08-06 16:18:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6057768840"))
 #Port
 PORT = os.environ.get("PORT", "8080")
 #Database
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://arman1262k:<password>@frootileaks.ghk8axd.mongodb.net/")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://arman1262k:EjrUxxctRDnU3JcT@frootileaks.ghk8axd.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "FrootiLeaks")
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002159880346"))
